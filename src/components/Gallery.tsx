eb6e04016f784b6d19a7b998f0c415d2bd732fd8
David Monfrooij <36758048+dmonfrooij@users.noreply.github.com>
2025-10-14 14:57:42 +0200

    Updated Gallery.tsx

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -16,19 +16,19 @@ const Gallery = () => {
       id: 2,
       title: "Bedrijfshal Dakbedekking",
       description: "Grote industriële hal voorzien van TPO kunststof dakbedekking",
-      image: "https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?auto=compress&cs=tinysrgb&w=800"
+      image: "src/photos/20250905_051929.jpg?auto=compress&cs=tinysrgb&w=800"
     },
     {
       id: 3,
       title: "Aanbouw Plat Dak",
       description: "Moderne aanbouw met hoogwaardige PVC dakbedekking",
-      image: "https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg?auto=compress&cs=tinysrgb&w=800"
+      image: "src/photos/20240108_052949_Gallery.jpg?auto=compress&cs=tinysrgb&w=800"
     },
     {
       id: 4,
-      title: "Garage Dakrenovatie",
-      description: "Volledige renovatie garagedak met EPDM rubber",
-      image: "https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=800"
+      title: "Dakrenovatie",
+      description: "Volledige renovatie garagedak met PVC",
+      image: "src/photos/20250314_182547.jpg?auto=compress&cs=tinysrgb&w=800"
     },
     {
       id: 5,
