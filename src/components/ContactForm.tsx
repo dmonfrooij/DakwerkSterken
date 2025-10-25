dc4eed1515d54662af77ac98ad5536f251d1397b
David Monfrooij <d.monfrooij@gmail.com>
2025-10-25 18:12:10 +0200

    google tag

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -23,7 +23,7 @@ const ContactForm = () => {
     setStatus("sending");
 
     try {
-      const res = await fetch("http://localhost:5000/api/send-mail", {
+      const res = await fetch("/api/send-mail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
